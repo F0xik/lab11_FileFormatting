9d51d945a3a7ccd297dc9990e7a5d774c3534051
Foxikg <Foxik@mail.ru>
2023-04-06 22:14:24 +0300

    lab11

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using static System.Net.Mime.MediaTypeNames;
+
+string path = "D:\\project c++\\ConsoleApp13\\ConsoleApp13\\bin\\Debug\\schedule.csv";
+// асинхронное чтение
+using (StreamReader reader = new StreamReader(path))
+{
+   
+   string text =  reader.ReadToEnd();
+
+
+
+
+
+    text = text.Replace(",", "");// замена , на ""
+
+    text = text.Replace("Группа - ", ""); // удаление Группа - 
+    text = text.Replace("", "");
+
+
+    Console.WriteLine(text);    // 
+
+
+
+
+
+
+
+
+
+}
+
